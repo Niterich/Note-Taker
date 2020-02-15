b8caa0ddb992d7294fda4d795752c56c717f8818
Niterich <52295078+Niterich@users.noreply.github.com>
2020-02-15 11:57:09 -0700

    functionality working as intended, app can write, save, and delete notes. finally deploying to heroku

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const nodemon = require("nodemon");
 const fs = require("fs");
 const db = require("./db/db.json");
+let idNumber;
 
 // Sets up the Express App
 // =============================================================
@@ -14,15 +15,9 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static(__dirname + "/"));
 app.use(express.static("public"));
 
 //Routes
-app.get("/", function(req, res) {
-    console.log(db);
-    res.json(db);
-    // res.sendFile(path.join(__dirname, "index.html"))
-});
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
@@ -33,36 +28,58 @@ app.get("/api/notes", function(req, res) {
 });
 
 app.post("/api/notes", function(req, res) {
-    res.sendFile(path.resolve("./db/db.json"))
-    console.log(req.body)
+    res.sendFile(path.resolve("./db/db.json"));
     writeNote(req.body);
 });
 
-app.get("/api/notes/:id", function(req, res) {
-    res.sendFile(path.join(__dirname, ""))
+app.delete("/api/notes/:id", function(req, res) {
+    idNumber = parseInt(req.params.id);
+    deleteNote(idNumber);
+    res.sendFile(path.resolve("./db/db.json"));
+});
+
+app.get("*", function(req, res) {
+    console.log(db);
+    res.json(db);
 });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
 
-// function writeNote() {
-    
-// }
-
 function writeNote(note){
+    let newNote = note;
+    let noteId = 1;
+    if (note.length > 0) {
+        noteId = note.pop().id;
+    }
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         let dbArray = JSON.parse(data);
-        console.log(typeof data);
-        dbArray.push(note);
-        console.log(dbArray);
+        newNote.id = dbArray.length + 1;
+        dbArray.push(newNote);
         fs.writeFile('./db/db.json', JSON.stringify(dbArray), (err, data) => {
             if (err) throw err;
+            return JSON.stringify(dbArray);
         })
     });
 }
 
-// find a way to assign a unique id to each note in order to search for and delete
-    // id of last piece of data and add 1
-// post api/notes when new note is saved, resend new updated db.json file to the page in order to refresh the note list on the left side of the page
+function deleteNote(id){
+    fs.readFile('./db/db.json', 'utf-8', (err, json) => {
+        const newNote = JSON.parse(json);
+        console.log(newNote)
+        for (let i = 0; i < newNote.length; i++) {
+            if (newNote[i].id === idNumber) {
+                newNote.splice(i, 1);
+                console.log(newNote);
+            }
+        }
+        fs.writeFile('./db/db.json', JSON.stringify(newNote), (err) => {
+            if (err) {
+                throw err;
+            }
+            return JSON.stringify(newNote);
+        })
+    })
+}
